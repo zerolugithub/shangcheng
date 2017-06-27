794b55660fa2892946155383e013d97bb902315f
BlackSimpler <657141755@qq.com>
2017-06-27 23:23:09 +0800

    更新权限的修改功能

--- Application/Admin/Controller/PrivilegeController.class.php
+++ Application/Admin/Controller/PrivilegeController.class.php
@@ -44,24 +44,46 @@ class PrivilegeController extends CommonController
     public function edit()
     {
         //获取修改权限信息ID
-        $priv_id=I('id');
+        $priv_id = I('id');
         //获取对应权限信息
         $privModel = D('Privilege');
-        if(IS_POST){
-            $privId = I('priv_id');
+        if (IS_POST) {
+            $id = I('priv_id');//获取id
+            $pid = I('pid');//获取提交的pid
+            //查询当前id权限下是否还有子级
+            $privDatas = $privModel->select();
+            //无限极分类查询子级
+            $sonData = getTree($privDatas,$id);
             $priv = I();
-            $res = $privModel->where(array('id'=>$privId))->save($priv);
-            if($res!==false){
-                $this->success('修改成功！',U('lst'));exit;
-            }else{
-                $this->error('修改失败');
+            if($id==$pid){
+                echo json_encode(array('status' => 0 , 'info' => '所选择的上级分类不能是当前分类或者当前分类的下级分类!' , 'url' => ''));
+                exit;
+            }
+            foreach($sonData as $son){
+                if($pid =$son['id']){
+                    echo json_encode(array('status' => 0 , 'info' => '所选择的上级分类不能是当前分类或者当前分类的下级分类!' , 'url' => ''));
+                    exit;
+                }
+            }
+            $res = $privModel -> where(array('id' => $id)) -> save($priv);
+            if ($res !== false) {
+                echo json_encode(array('status' => 1 , 'info' => '修改成功!' , 'url' => U('lst')));
+                exit;
+            } else {
+                echo json_encode(array('status' => 0 , 'info' => '所选择的上级分类不能是当前分类或者当前分类的下级分类!' , 'url' => ''));
             }
         }
-        $privData = $privModel->find($priv_id);
-        $this->assign('privData',$privData);
+        $privData = $privModel -> find($priv_id);
+        $this -> assign('privData' , $privData);
         //获取权限等级信息并在视图中显示
         $privTree = $privModel -> privTree();
         $this -> assign('privTree' , $privTree);
-        $this->display();
+        $this -> display();
+    }
+
+    //删除权限（栏目）
+    public function del()
+    {
+
     }
 }
--- Application/Runtime/Cache/Admin/1c990affb7d51d2df56dce018d5c78a2.php
+++ Application/Runtime/Cache/Admin/1c990affb7d51d2df56dce018d5c78a2.php
@@ -41,7 +41,7 @@
                     </td>
                     <td align="center">
                         <a href="<?php echo U('edit','id='.$val[id]);?>">编辑</a> |
-                        <a href="javascript:void(0)" title="移除" data_id="<?php echo ($cate[id]); ?>" id="del">移除</a>
+                        <a href="javascript:void(0)" title="移除" data_id="<?php echo ($val[id]); ?>" id="del">移除</a>
                     </td>
                 </tr><?php endforeach; endif; ?>
         </table>
@@ -59,7 +59,7 @@
                 var _this=$(this);
                 var id=_this.attr('data_id');
                 console.log(id);
-                $.post('<?php echo U("category/del");?>',{'id':id},function(data){
+                $.post('<?php echo U("privilege/del");?>',{'id':id},function(data){
                     if(data['status']==1){
                         layer.msg(data['info']);
                         _this.parents('tr').remove();
