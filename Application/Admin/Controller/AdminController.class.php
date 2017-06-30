34733ab48e6ec6076dcd36d2e6bf712c6b30dacd
BlackSimpler <657141755@qq.com>
2017-06-30 13:50:21 +0800

    前台布局测试

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -36,14 +36,56 @@ class AdminController extends CommonController
     //显示管理员信息
     public function lst()
     {
-        $adminModel=D('Admin');
-        $adminData=$adminModel->where(array('is_admin'=>0))->select();
-        $this->assign('adminData',$adminData);
-        $this->display();
+        $adminModel = D('Admin');
+        $adminData = $adminModel -> where(array('is_admin' => 0)) -> select();
+        $this -> assign('adminData' , $adminData);
+        $this -> display();
     }
 
     public function del()
     {
 
     }
+
+    //管理员编辑
+    public function edit()
+    {
+        //获取管理员id
+        $admin_id = I('id');
+        //管理员信息
+        $adminModel = D('Admin');
+        if(IS_POST){
+//            if(D(''))
+        }
+        $adminInfo = $adminModel->join("a left join mall_admin_role b on a.id=b.admin_id")->field("a.*,b.role_id")->where(array('a.id'=>$admin_id))->find();
+//        p($adminInfo);die;
+        $this->assign('adminInfo',$adminInfo);
+        //获取角色表信息
+        $roleData = D('Role')->select();
+        $this->assign('roleData',$roleData);
+        $this->display();
+    }
+
+    //权限分配
+    /* public function priv()
+     {
+         if(IS_POST){
+
+
+         }
+         //获取用户id
+         $uid = I('id');
+         //用户权限信息id
+         $privInfo = M('AdminRole')->field('b.priv_id')->join("a left join mall_role_privilege b on a.role_id=b.role_id")->where('a.admin_id='.$uid)->select();
+         foreach ($privInfo as $val){
+             $privLst[]=$val['priv_id'];
+         }
+         //将权限ID传到前台模型 与权限做对比，如果权限ID相同就默认勾选
+         $this->assign('privLst',$privLst);
+         //获取权限信息
+         $privModel = D('Privilege');
+         $privData = $privModel -> privNavTree();
+         $this -> assign('privData' , $privData);
+         $this->display();
+     }*/
 }
